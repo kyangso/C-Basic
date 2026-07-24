c8545df6b5364b953c23a75d71c0c018ef826123
kyangso <wonjaechoi15@gmail.com>
2026-07-24 05:01:11 +0000

    20260724

---
+++ dweight.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    // int height;
+    // int length;
+    // int width;
+    // int volume;
+    // int weight;
+
+    // height = 8;
+    // length = 12;
+    // width = 10;
+
+    // volume = height * length * width;
+    // weight = (volume + 165) / 166;
+
+    // printf("Dimesions: %dx%dx%d\n", length, width, height);
+    // printf("Volume (cubic inches): %d\n", volume);
+    // printf("Dimensional weight (pounds): %d\n", weight);
+
+
+ // ================================================================================= // 
+
+    return 0;
+}
+
+
