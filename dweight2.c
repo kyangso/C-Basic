344eaf11b3cbc4720f8372c6ac3799ed2fada6b2
kyangso <wonjaechoi15@gmail.com>
2026-07-27 05:04:00 +0000

    20260727

--- dweight2.c
+++ dweight2.c
@@ -8,11 +8,11 @@ int main(void) {
     int weight;
 
     printf("Enter height of box: ");
-    scanf("%d", &height);
+    scanf("%d", &height);                           // int로 저장하기 때문에 &d를 사용, %의 경우 주소를 뜻함 
     printf("Enter length of the box: ");
     scanf("%d", &length);
     printf("Enter width of box: ");
-    scanf("%d", *&width);
+    scanf("%d", &width);
 
     volume = height * length * width;
     weight = (volume + 165) / 166;
@@ -22,4 +22,3 @@ int main(void) {
 
     return 0;
 }
-
