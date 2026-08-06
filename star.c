920d69aea8fe219bf2b1584a5004f73ce2aa9724
kyangso <wonjaechoi15@gmail.com>
2026-08-06 04:59:59 +0000

    20260806

--- star.c
+++ star.c
@@ -2,8 +2,12 @@
 
 int main(void) {
 
-    int i;
-
+    for(int i = 1;i <= 6; i++) {
+        // printf("%d ", i);
+        for(int j = 1;  ;j++) {         // i = j 를 조건으로 만들어라. i 가 1일때 1번 반복 이렇게.. 검증해라
+            printf("*");
+        }
+    }
 
     return 0;
 }
