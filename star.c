8f76d6f12916139035dfcfe04625e7f42372bb02
kyangso <wonjaechoi15@gmail.com>
2026-08-07 04:58:21 +0000

    20260807

--- star.c
+++ star.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-
     for(int i = 1;i <= 6; i++) {
-        // printf("%d ", i);
-        for(int j = 1;  ;j++) {         // i = j 를 조건으로 만들어라. i 가 1일때 1번 반복 이렇게.. 검증해라
-            printf("*");
+        for(int j = 1;j <= i;j++) {     //  "조건" 과 변수를 헷갈리지 말자. 중첩 반복문은 둘중 아니면 셋중 어느것도 쓸모가 없진 않다
+            printf("* ");
         }
+        printf("\n");
     }
-
     return 0;
 }
